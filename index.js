05535664e3f7ec25a1f4ea33d6f52f14efe96bf5
brebz7 <andrei_brebeanu@yahoo.com>
2019-08-24 22:45:38 +0300

    Added a message when some user is typing

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(express.static('public'));
 const io = socket(server);
 
 io.on('connection', function (socket) {
-  console.log('made socket connection');
-
-  socket.on('chat', function(data){
+  socket.on('chat', function (data) {
     io.sockets.emit('chat', data)
+  });
+
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data)
   })
 })
