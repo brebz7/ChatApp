fe99afa91a5acd1bb214234fdf5e6afa05b96cfc
brebz7 <andrei_brebeanu@yahoo.com>
2019-08-24 21:58:41 +0300

    added socketio on the server and client. made a simple connection

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const socket = require('socket.io');
+
 const app = express();
 const server = app.listen(4000, () => console.log('ChatApp is listening on port 4000'));
 
 app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', function (socket) {
+  console.log('made socket connection');
+})
